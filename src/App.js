2c88ba2fd18addebeb8ffffa4f9dce488b824233
Irfan Ansari <Irfan.Ansari@blueconchtech.com>
2021-12-15 11:07:05 +0530

    File and Folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from "react";
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
 import { Article, Brand, CTA, Feature, Navbar } from "./components";
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      <div className="gradient_bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
       <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,11 @@
+import "./whatGPT3.css";
+
+const WhatGPT3 = () => {
+  return (
+    <div>
+      <h1>whatGPT3</h1>
+    </div>
+  );
+};
+
+export default WhatGPT3;
